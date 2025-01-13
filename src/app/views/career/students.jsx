e3694c3d94d676edbb57fcd283093afedd1f4f05
avanegas <arco527@gmail.com>
2025-01-13 10:03:46 -0500

    Add email and styles

--- src/app/views/career/students.jsx
+++ src/app/views/career/students.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState} from 'react';
-import { Button } from '@material-ui/core'
+import React, { useEffect, useState } from 'react';
+import { Avatar, Button } from '@material-ui/core'
 import { Breadcrumb } from 'matx';
 import { Link } from 'react-router-dom';
 import { SmartMUIDataTable } from 'app/components/SmartDataTable';
@@ -7,15 +7,15 @@ import bc from '../../services/breathecode';
 import { id } from 'date-fns/locale';
 // import { viewColumns } from '@material-ui/icons';
 
-const Students = () =>{
-    const [items, setItems]= useState([])
+const Students = () => {
+    const [items, setItems] = useState([])
 
     // useEffect(() => {
     //     const fetchStudents = async () => {
     //         try {
     //             const { data } = await bc.auth().getAcademyStudents();
     //             console.log("Data:", data)
-                
+
     //             setItems(data);
     //         } catch (error){
     //             console.error('Error fetching students:', error)
@@ -29,62 +29,70 @@ const Students = () =>{
     }, [items])
 
     return (
-        <div className="mb-sm-30" style={{margin: "20px"}}>
-            <div className="flex flex-wrap justify-between mb-6">
+        <div className="mb-sm-30" style={{ margin: "20px" }}>
+            {/* <div className="flex flex-wrap justify-between mb-6">
                 <div>
                     <Breadcrumb routeSegments={[{ name: 'Career', path: '/students/career' }, { name: 'Students' }]} />
                 </div>
-    
+
                 <div className="">
                     <Link to="/students/new">
-                    <Button variant="contained" color="primary">
-                        Add new Student
-                    </Button>
+                        <Button variant="contained" color="primary">
+                            Add new Student
+                        </Button>
                     </Link>
                 </div>
-            </div>
+            </div> */}
 
             <div>
-                    <SmartMUIDataTable
-                        title='All Students Graduated'
-                        columns={[
-                            {
-                                name: 'first_name',
-                                label: 'Name',
-                                options: {                                    
-                                    customBodyRenderLite: (dataIndex) => (
+                <SmartMUIDataTable
+                    title='All Students Graduated'
+                    columns={[
+                        {
+                            name: 'first_name',
+                            label: 'Name',
+                            options: {
+                                customBodyRenderLite: (dataIndex) => (
+                                    <div className='flex items-center'>
+                                        <Avatar className='w-48 h-48' src={items[dataIndex]?.user.profile.avatar_url}/>
                                         <div>
-                                            {items[dataIndex]?.profile_academy.first_name} {items[dataIndex]?.profile_academy.last_name}
+                                            <h5 className='ml-3 my-0 text-15'>
+                                                {items[dataIndex]?.profile_academy.first_name} {items[dataIndex]?.profile_academy.last_name}
+                                            </h5>
+                                            <small className='text-muted ml-3'>
+                                                {items[dataIndex]?.profile_academy.email || items[dataIndex].profile_academy.email}
+                                            </small>
                                         </div>
-                                    ),
-                                },
+                                    </div>
+                                ),
                             },
-                            {
-                                name: 'created_at',
-                                label: 'Created At',
-                                options: {
-                                    customBodyRenderLite: (dataIndex) => (
-                                        <div>{new Date(items[dataIndex]?.created_at).toLocaleDateString()}</div>
-                                    ),
-                                },
+                        },
+                        {
+                            name: 'created_at',
+                            label: 'Created At',
+                            options: {
+                                customBodyRenderLite: (dataIndex) => (
+                                    <div>{new Date(items[dataIndex]?.created_at).toLocaleDateString()}</div>
+                                ),
                             },
-                        ]}
-                        items={items}
-                        options={{
-                            print:false,
-                            viewColumns: false,
-                        }}
-                        search={async (querys) => {
-                            const finalQuerys = {
-                                educational_status: 'graduated',
-                                ...querys,
-                            }
-                            const { data } = await bc.admissions().getAllUserCohorts(finalQuerys);
-                            console.log("DATA", data)
-                            setItems(data.results);
-                            return data
-                        }}
-                    />
+                        },
+                    ]}
+                    items={items}
+                    options={{
+                        print: false,
+                        viewColumns: false,
+                    }}
+                    search={async (querys) => {
+                        const finalQuerys = {
+                            educational_status: 'graduated',
+                            ...querys,
+                        }
+                        const { data } = await bc.admissions().getAllUserCohorts(finalQuerys);
+                        console.log("DATA", data)
+                        setItems(data.results);
+                        return data
+                    }}
+                />
             </div>
         </div>
     )
